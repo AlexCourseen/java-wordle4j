2a35681015b949cbc86402d0b264b4d6bc0d69a9
aleksandr.kursin <beardkid7@gmail.com>
2026-02-15 15:59:20 +0300

    edit after test

---
+++ src/ru/yandex/practicum/Log.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+public class Log implements AutoCloseable {
+
+    private final PrintWriter writer;
+
+    Log(String fileLog) throws IOException {
+        this.writer = new PrintWriter(new FileWriter(fileLog, StandardCharsets.UTF_8));
+    }
+
+    public void toLog(Exception e) {
+        e.printStackTrace(writer);
+    }
+
+    @Override
+    public void close() {
+        if (writer != null) {
+            writer.close();
+        }
+    }
+}
+
+
